a6520e3cc33152e8c467c7bcf08b3e0d3b54a99f
AyuAngreini <33110195+AyuAngreini@users.noreply.github.com>
2017-12-12 21:26:32 -0800

    [ayu]

--- home/web_detail/cara_pesan.php
+++ home/web_detail/cara_pesan.php
@@ -1,8 +1,16 @@
     <div class="col-sm-7 mt50">
             <h2 class="lined-heading"><span>Cara Pesan</span></h2>
             <ol>
+              <li>Buka website “saferental” di smartphone Anda.</li>
+
+              <li>SPilih jenis layanan GrabCar dengan ikon bergambar mobil pribadi.</li>
+              
+              <li>Jika ada pembatalan unit mobil yang telah dipesan atau perubahan jadwal harap diberitahukan lebih dari 1 x 24 jam sebelumnya dan kami akan mentransfer kembali DP customer max 2 x 24 jam dari waktu pembatalan. DP akan hangus jika pembatalan sewa dilakukan kurang dari 1 x 24 jam dari jam penjemputan.</li>
+              
               <li>Customer dapat melakukan blocking mobil dengan membayar DP50,000/ booking.</li>
+              
               <li>Sisa pembayaran dapat dilakukan pada saat mobil yang dipesan diambil di tempat rental.</li>
+              
               <li>Jika ada pembatalan unit mobil yang telah dipesan atau perubahan jadwal harap diberitahukan lebih dari 1 x 24 jam sebelumnya dan kami akan mentransfer kembali DP customer max 2 x 24 jam dari waktu pembatalan. DP akan hangus jika pembatalan sewa dilakukan kurang dari 1 x 24 jam dari jam penjemputan.</li>
             </ol>
           </div>
